ce17d292de0392bc246f74a06ae68d0d992b16dc
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-03-06 18:50:13 -0600

    Commands to check the status of a specific suscription

--- implement-message-workflows-with-service-bus/src/start/get-queue-info.sh
+++ implement-message-workflows-with-service-bus/src/start/get-queue-info.sh
@@ -13,3 +13,19 @@ az servicebus queue show \
     --name salesmessages \
     --query messageCount \
     --namespace-name salesteamappalvaroruano
+
+# Show messages pending on the topic for Americas subscription
+az servicebus topic subscription show \
+    --resource-group learn-2d55abc9-46a1-4577-bf0c-60c42340ddb3 \
+    --namespace-name salesteamappalvaroruano \
+    --topic-name salesperformancemessages \
+    --name Americas \
+    --query messageCount
+
+# Show messages pending on the topic for Americas subscription
+az servicebus topic subscription show \
+    --resource-group learn-2d55abc9-46a1-4577-bf0c-60c42340ddb3 \
+    --namespace-name salesteamappalvaroruano \
+    --topic-name salesperformancemessages \
+    --name EuropeAndAfrica \
+    --query messageCount
